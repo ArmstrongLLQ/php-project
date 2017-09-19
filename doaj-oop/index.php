072971171d1e1909c441d10eb965698286f9393f
ArmstrongLLQ <armstrong_lab@163.com>
2017-09-19 17:15:02 +0800

    sync

--- doaj-oop/index.php
+++ doaj-oop/index.php
@@ -1,18 +1,5 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>doaj</title>
-    <link rel="stylesheet" type="text/css" href="jquery-easyui-1.5.2/themes/default/easyui.css">
-    <link rel="stylesheet" type="text/css" href="jquery-easyui-1.5.2/themes/icon.css">
-    <link rel="stylesheet" type="text/css" href="jquery-easyui-1.5.2/demo/demo.css">
-    <script type="text/javascript" src="jquery-easyui-1.5.2/jquery.min.js"></script>
-    <script type="text/javascript" src="jquery-easyui-1.5.2/jquery.easyui.min.js"></script>
-</head>
-<body>
 
 <?php
 header("Location:MainPage.php");
 exit();
 ?>
-</body>
