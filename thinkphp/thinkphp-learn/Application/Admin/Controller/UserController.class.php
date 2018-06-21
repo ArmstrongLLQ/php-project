d34fb567e017da3b360b5c9eca7cae9730c13d6f
ArmstrongLLQ <armstrong_lab@163.com>
2018-06-21 21:49:35 +0800

    sync

--- thinkphp/thinkphp-learn/Application/Admin/Controller/UserController.class.php
+++ thinkphp/thinkphp-learn/Application/Admin/Controller/UserController.class.php
@@ -3,10 +3,27 @@ namespace Admin\Controller;
 use Think\Controller;
 class UserController extends Controller{
 	public function showList(){
+		$model = M('User');
+		$data = $model->select();
+		$this->assign('data', $data);
 		$this->display();
 	}
 
 	public function add(){
-		$this->display();
+		if(IS_POST){
+			$model = M('User');
+			$data = $model->create();
+			$data['addtime'] = time();
+			$result = $model->add($data);
+			if($result){
+				$this->success('添加成功', U('showList'), 3);
+			}else{
+				$this->error('添加失败');
+			}
+		}else{
+			$data = M('Dept')->field('id, name')->select();
+			$this->assign('data', $data);
+			$this->display();
+		}
 	}
 }
